664b7d49247e6a330693a992f9d501533721d5a5
jimd1989 <jamesdalrymple@protonmail.com>
2024-01-13 18:54:29 +1000

    ugly working fixed point lerp

--- src/waves/sine.c
+++ src/waves/sine.c
@@ -42,13 +42,20 @@ F16_16 sine(UF16_16 phase) {
   //F16_16 r = F16_16_FRAC(phase);
   uint32_t MASK = ((1 << (30 - 10)) - 1);
   //0.00000095367522590182
+  //0.00001525902189669642
   float x = (BACKWARDS(phase) & MASK) / (float)MASK;
   F16_16 r = f16_16_mult(s2 - s1, f16_16(x));
-  //warnx("%f %u %f %u", x, f16_16(x), f16_16_float(F16_16_FRAC(BACKWARDS(phase))), F16_16_FRAC(BACKWARDS(phase)));
+  //F16_16 xx = f16_16_mult(F16_16_FRAC(BACKWARDS(phase)), f16_16(0.00001525902189669642f));
+  //F16_16 xx = (int32_t)(F16_16_TO_F1_16((int64_t)(F16_16_FRAC(BACKWARDS(phase)) << 16) / f16_16(F16_16_FRAC_MASK)));
+  F16_16 xx = f16_16_mult(BACKWARDS(phase) & 1048575, 4096);
+  F16_16 rr = f16_16_mult(s2 - s1, xx);
+  //warnx("%u %u %f", f16_16(x), xx, f16_16_float(128));
+  //warnx("%f %f", f16_16_float(xx), f16_16_float(f16_16_mult(xx, F16_16_FRAC_MASK)));
+  //warnx("%u\t%u\t%u\t%u\t%f\t%f", f16_16(x), r, xx, rr, f16_16_float(r), f16_16_float(rr)); 
   //F16_16 recp = f16_16(0.95367522590182f);
   //F16_16 r = f16_16_mult(BACKWARDS(phase) & MASK, recp);
   //r = f16_16_mult(s2 - s1, r);
-  s1 += r;
+  s1 += rr;
   return NEGATE(s1, phase);
 }
 
